83060605a7d8f8af6bd8fbeb2df6cd96d35b9886
Oka <okaaamhdt1099@gmail.com>
2024-12-04 13:03:33 +0700

    feat: context

--- client/chating-app/src/main.jsx
+++ client/chating-app/src/main.jsx
@@ -5,12 +5,16 @@ import App from "./App.jsx";
 import store from "./app/store.js";
 import { Provider } from "react-redux";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import ThemeContext from "../../../../live-chat/client/chating-app/src/context/ThemeContext.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="383800483107-grpgn26t7p2ts17qefd1fkhe32ac4ppl.apps.googleusercontent.com">
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <ThemeContext> 
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ThemeContext>
     </GoogleOAuthProvider>
   </StrictMode>
 );
